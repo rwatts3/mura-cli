d690ef07519f058cefd696446ef3a9f5a82e4da7
Ryan Watts <ryandwatts@gmail.com>
2016-02-16 01:43:29 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,89 +1,36 @@
 #!/usr/bin/env node
+
 // require
 var cli = require('cli').enable('version');
 var shell = require('shelljs');
 var prompt = require('prompt');
 var fs = require('fs');
 
-cli.setApp('lc', '0.1.0');
+// app details
+cli.setApp('mura', '0.0.1');
 
+// options
 var options = {
-    // options
     file:	['f', 'A file to process', 'file', 'temp.log'],
     time:	['t', 'An access time', 'time', false],
-    work:	[false, 'What kind of work to do', 'string', 'sleep'],
-    box:	['b', 'Chooses between Node or CF but defaults to Node', 'string', 'node'],
-	client: ['c', 'Client to Update', 'string', false]
-	
+    work:	[false, 'What kind of work to do', 'string', 'sleep']
 };
 
-var commands = ['stopAll', 'startAll','promptTest', 'svnUpdate'];
+// commands
+var commands = [
+	'gitTest'
+];
 
 // register options and commands.
 cli.parse(options, commands);
 
-if (cli.command == 'promptTest') {
-	prompt.get(['username', 'password'], function (err, result) {
-		console.log(result.username);
-		console.log(result.password);
-	});
-}
+// friendly message
+console.log('MURA CMS CLI'.bold.green + ' Unofficial'.zebra);
 
 
 /****************************************
 * Private FUNCTIONS AND COMMANDS BELOW  *
 ****************************************/
-
-// SVN Update Client
-
-if (cli.command == 'svnUpdate' && cli.options.client) {
-	shell.cd('d:/cms30/nodeServer/clients/');
-	shell.exec('svn update ' + cli.options.client);
-}
-
-// Suspend All Node Boxes
-if (cli.command == 'stopAll' && cli.options.box == 'node') {
-	cli.info('Suspending Node Boxes');
-
-	shell.cd('d:/cms30/nodeServer');
-	shell.exec('vagrant suspend');
-
-	shell.cd('d:/cms30/mongoServer');
-	shell.exec('vagrant suspend');
-
-	shell.cd('d:/cms30/solrServer');
-	shell.exec('vagrant suspend');
-
-	cli.ok('All Boxes Suspended');
-}
-
-// Start All Node Boxes
-if (cli.command == 'startAll' && cli.options.box == 'node') {
-	cli.info('Starting Node Boxes');
-
-	shell.cd('d:/cms30/nodeServer');
-	shell.exec('vagrant up');
-
-	shell.cd('d:/cms30/mongoServer');
-	shell.exec('vagrant up');
-
-	shell.cd('d:/cms30/solrServer');
-	shell.exec('vagrant up');
-
-	cli.ok('All Boxes Running');
-}
-
-// Stop CF Boxes
-if (cli.command == 'stopAll' && cli.options.box == 'cf') {
-	cli.info('Stopping CF');
-	shell.exec('net stop "ColdFusion 9 Application Server"');
-	shell.exec('net stop "ColdFusion 9 ODBC Agent"');
-	shell.exec('net stop "ColdFusion 9 ODBC Server"');
-	shell.exec('net stop "ColdFusion 9 Search Server"');
-	shell.exec('net stop "ColdFusion 9 .NET Service"');
-	shell.exec('net stop "ColdFusion 9 Solr Service"');
-	cli.ok('All CF Boxes Stopped');
-}
 
 // Start CF Boxes
 if (cli.command == 'startAll' && cli.options.box == 'cf') {
@@ -97,4 +44,9 @@ if (cli.command == 'startAll' && cli.options.box == 'cf') {
 	cli.ok('All CF Boxes Running');
 }
 
-
+if (cli.command == 'promptTest') {
+	prompt.get(['username', 'password'], function (err, result) {
+		console.log(result.username);
+		console.log(result.password);
+	});
+}
